11b10bb24c870f94964728e72fba8e140e5ee3e0
Ken-Alviola <kenalviola@gmail.com>
2021-04-09 16:28:13 -0500

    begin explore

--- acquire.py
+++ acquire.py
@@ -4,7 +4,7 @@ import pandas as pd
 def acquire_semicon():
     df =  pd.read_csv('secom.data', sep=' ', header=None)
     labels = pd.read_csv('secom_labels.data', sep=' ', header=None)
-
+        
     df['timestamp'] = labels[1]
     df['defect'] = labels[0]
     df.defect = df.defect.apply(lambda x: 0 if x == -1 else 1)
