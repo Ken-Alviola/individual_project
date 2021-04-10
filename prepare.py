df437e6b3e4029119848f0e03565a3281795b952
Ken-Alviola <kenalviola@gmail.com>
2021-04-10 17:56:07 -0500

    done modeling

--- prepare.py
+++ prepare.py
@@ -71,3 +71,26 @@ def scale_data_minmax(train,validate,test):
     test_scaled = pd.DataFrame(test_scaled, columns=train.columns)
     
     return train_scaled, validate_scaled, test_scaled
+
+def get_metrics(model, X, y):
+    '''
+    get_metrics_bin will take in a sklearn classifier model, an X and a y variable and utilize
+    the model to make a prediction and then gather accuracy, class report evaluations
+    return:  a classification report as a pandas DataFrame
+    '''
+    y_pred = model.predict(X)
+    accuracy = model.score(X, y)
+    conf = confusion_matrix(y, y_pred)
+    print('confusion matrix: \n', conf)
+    print()
+    class_report = pd.DataFrame(classification_report(y, y_pred, output_dict=True)).T
+    tpr = conf[1][1] / conf[1].sum()
+    fpr = conf[0][1] / conf[0].sum()
+    tnr = conf[0][0] / conf[0].sum()
+    fnr = conf[1][0] / conf[1].sum()
+    print(f'''
+    The accuracy for our model is {accuracy:.4}
+    The True Positive Rate is {tpr:.3}, The False Positive Rate is {fpr:.3},
+    The True Negative Rate is {tnr:.3}, and the False Negative Rate is {fnr:.3}
+    ''')
+    return class_report
